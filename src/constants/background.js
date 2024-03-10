a18367bc6df947838abd7fbc06ab04c646e16388
suhjuho <sspaka0422@gmail.com>
2024-03-11 01:16:25 +0900

    Feat: add background, cube constant

---
+++ src/constants/background.js
@@ -0,0 +1,5 @@
+const BACKGROUND_CONSTANT = {
+  MATERIAL_ARGS: { color: "#577D6D" },
+};
+
+export default BACKGROUND_CONSTANT;
--- src/constants/cube.js
+++ src/constants/cube.js
@@ -9,6 +9,16 @@ const CUBE_CONSTANT = {
     [0, -Math.PI / 2, 0],
     [0, 0, 0],
   ],
+  MATERIAL_ARGS: {
+    blank: { color: "#ffffff" },
+    marked: { color: "#55cd55" },
+    invisible: {},
+    haze: { opacity: 0.2, color: "#96c1f0" },
+  },
+  HOVER_MATERIAL_ARGS: {
+    hover: { emissive: "#5bea5b" },
+    default: {},
+  },
 };
 
 export default CUBE_CONSTANT;
