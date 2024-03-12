4ed22e3f22bfe8d66f894e9e06aeeb74312ce456
suhjuho <sspaka0422@gmail.com>
2024-03-13 05:34:12 +0900

    Feat: add event on rotate

--- src/components/GameStage/GameStage.jsx
+++ src/components/GameStage/GameStage.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
-import Puzzle from "../puzzles/Puzzle";
+import Puzzle from "../Puzzle/Puzzle";
 import BackGround from "./BackGround";
 import GameStageHeader from "../Header/GameStageHeader";
 import GameStageSideBar from "../SideBar/GameStageSideBar";
@@ -14,6 +14,8 @@ import {
   usePuzzlesStore,
   useClickModeStore,
   useOrbitControlStore,
+  useCameraPositionStore,
+  useLayerStore,
 } from "../../store/store";
 
 const Stage = styled.div`
@@ -25,8 +27,14 @@ function GameStage() {
   const { puzzles } = usePuzzlesStore();
   const { clickMode, setClickMode } = useClickModeStore();
   const { isOrbitEnable } = useOrbitControlStore();
+  const { setCameraPosition } = useCameraPositionStore();
+  const { layerDirection, setLayerDirection, setCurrentLayer, setLayers } =
+    useLayerStore();
   const { difficulty, stageNumber } = useParams();
   const controls = useRef();
+  const camera = useRef();
+
+  const puzzle = puzzles[difficulty][stageNumber];
 
   useEffect(() => {
     function handleContextMenu(event) {
@@ -43,7 +51,80 @@ function GameStage() {
     return () => window.removeEventListener("keydown", handleContextMenu);
   }, [clickMode]);
 
-  const puzzle = puzzles[difficulty][stageNumber];
+  function checkCameraPosition(cameraPosition) {
+    const posX = cameraPosition.x >= 0 ? 1 : 0;
+    const posY = cameraPosition.y >= 0 ? 1 : 0;
+    const posZ = cameraPosition.z >= 0 ? 1 : 0;
+
+    return [posX, posY, posZ];
+  }
+
+  function checkLayerDirection(cameraPosition) {
+    const X = cameraPosition.x;
+    const Z = cameraPosition.z;
+
+    if (Z < X && Z >= -1 * X) {
+      if (layerDirection !== "RIGHT") {
+        const newLayers = [];
+
+        for (let x = -1 * puzzle.size[0] + 1; x <= puzzle.size[0] - 1; x += 2) {
+          newLayers.push(x);
+        }
+
+        setCurrentLayer(puzzle.size[0]);
+        setLayers(newLayers);
+      }
+
+      setLayerDirection("RIGHT");
+    } else if (Z >= X && Z < -1 * X) {
+      if (layerDirection !== "LEFT") {
+        const newLayers = [];
+
+        for (let x = -1 * puzzle.size[0] + 1; x <= puzzle.size[0] - 1; x += 2) {
+          newLayers.push(x);
+        }
+
+        setCurrentLayer(1);
+        setLayers(newLayers);
+      }
+
+      setLayerDirection("LEFT");
+    } else if (Z >= X && Z >= -1 * X) {
+      if (layerDirection !== "FRONT") {
+        const newLayers = [];
+
+        for (let z = -1 * puzzle.size[2] + 1; z <= puzzle.size[2] - 1; z += 2) {
+          newLayers.push(z);
+        }
+
+        setCurrentLayer(puzzle.size[2]);
+        setLayers(newLayers);
+      }
+
+      setLayerDirection("FRONT");
+    } else if (Z < X && Z < -1 * X) {
+      if (layerDirection !== "BACK") {
+        const newLayers = [];
+
+        for (let z = -1 * puzzle.size[2] + 1; z <= puzzle.size[2] - 1; z += 2) {
+          newLayers.push(z);
+        }
+
+        setCurrentLayer(1);
+        setLayers(newLayers);
+      }
+
+      setLayerDirection("BACK");
+    }
+  }
+
+  const handleRotate = () => {
+    const cam = camera.current;
+    const position = cam.position.clone();
+
+    setCameraPosition(checkCameraPosition(position));
+    checkLayerDirection(position);
+  };
 
   return (
     <Stage>
@@ -58,6 +139,7 @@ function GameStage() {
         <directionalLight intensity={1} position={[10, 5, 10]} />
 
         <OrthographicCamera
+          ref={camera}
           makeDefault
           position={[-10, 10, 10]}
           fov={100}
@@ -75,6 +157,9 @@ function GameStage() {
           enableZoom={false}
           enablePan={false}
           enabled={isOrbitEnable}
+          enableDamping
+          dampingFactor={0.2}
+          onChange={handleRotate}
         />
       </Canvas>
     </Stage>
