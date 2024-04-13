fb8f69c52556970c67ecd9f9cb308e0baed26e42
suhjuho <sspaka0422@gmail.com>
2024-04-14 04:03:03 +0900

    Feat: add resizeobserver for test code

--- src/components/CustomPuzzle/CustomPuzzle.jsx
+++ src/components/CustomPuzzle/CustomPuzzle.jsx
@@ -1,5 +1,6 @@
 import { Canvas } from "@react-three/fiber";
 import { OrthographicCamera, OrbitControls } from "@react-three/drei";
+import { ResizeObserver } from "@juggle/resize-observer";
 import * as THREE from "three";
 import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
@@ -167,7 +168,7 @@ function CustomPuzzle() {
 
       <CustomPuzzleFooter customCubesState={customCubesState} />
 
-      <Canvas>
+      <Canvas resize={{ polyfill: ResizeObserver }}>
         <ambientLight intensity={1} />
         <pointLight position={[0, 15, 20]} />
         <directionalLight intensity={3} position={[-10, -8, -6]} />
