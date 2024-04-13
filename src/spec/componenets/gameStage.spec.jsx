51d7ffebdc4ef8289e18bc60a791dafd5253fb94
suhjuho <sspaka0422@gmail.com>
2024-04-14 04:07:36 +0900

    Test: refactor test for game stage

--- src/spec/componenets/gameStage.spec.jsx
+++ src/spec/componenets/gameStage.spec.jsx
@@ -1,7 +1,6 @@
-import { beforeEach, describe, expect, it, test, vi } from "vitest";
-import { render, screen, waitFor } from "@testing-library/react";
+import { describe, it } from "vitest";
+import { render } from "@testing-library/react";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
-import * as ReactRouter from "react-router-dom";
 import GameStage from "../../components/GameStage/GameStage";
 import TutorialGameStage from "../../components/GameStage/TutorialGameStage";
 import usePuzzlesStore from "../../store/puzzle";
@@ -10,34 +9,36 @@ import puzzleMock from "../puzzleMock.json";
 
 const initialState = usePuzzlesStore.getState();
 
-test("enter the game stage", () => {
-  initialState.puzzles.easy = puzzleMock.easy;
-  usePuzzlesStore.setState(initialState);
+describe("render game State", () => {
+  it("enter the game stage", () => {
+    initialState.puzzles.easy = puzzleMock.easy;
+    usePuzzlesStore.setState(initialState);
 
-  render(
-    <MemoryRouter initialEntries={["/puzzles/easy/1"]}>
-      <Routes>
-        <Route
-          path="/puzzles/:difficulty/:stageNumber"
-          element={<GameStage />}
-        />
-      </Routes>
-    </MemoryRouter>,
-  );
-});
+    render(
+      <MemoryRouter initialEntries={["/puzzles/easy/1"]}>
+        <Routes>
+          <Route
+            path="/puzzles/:difficulty/:stageNumber"
+            element={<GameStage />}
+          />
+        </Routes>
+      </MemoryRouter>,
+    );
+  });
 
-test("enter the game stage", () => {
-  initialState.puzzles.tutorial = puzzleMock.easy;
-  usePuzzlesStore.setState(initialState);
+  it("enter the tutorial game stage", () => {
+    initialState.puzzles.tutorial = puzzleMock.easy;
+    usePuzzlesStore.setState(initialState);
 
-  render(
-    <MemoryRouter initialEntries={["/puzzles/tutorial/1"]}>
-      <Routes>
-        <Route
-          path="/puzzles/tutorial/:stageNumber"
-          element={<TutorialGameStage />}
-        />
-      </Routes>
-    </MemoryRouter>,
-  );
+    render(
+      <MemoryRouter initialEntries={["/puzzles/tutorial/1"]}>
+        <Routes>
+          <Route
+            path="/puzzles/tutorial/:stageNumber"
+            element={<TutorialGameStage />}
+          />
+        </Routes>
+      </MemoryRouter>,
+    );
+  });
 });
