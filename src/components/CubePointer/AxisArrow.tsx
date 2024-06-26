2f44dbb2101f0d4afe15eb906f09636951824cd8
suhjuho <sspaka0422@gmail.com>
2024-06-26 17:02:52 +0900

    Refactor: migrate typescript axis 3d component

--- src/components/CubePointer/AxisArrow.tsx
+++ src/components/CubePointer/AxisArrow.tsx
@@ -1,26 +1,37 @@
 import React, { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
+import { Mesh, MeshStandardMaterial } from "three";
+import { Coordinate } from "../../../types/cube.ts";
 
 useGLTF.preload("/assets/arrow_3d.glb");
 
-function AxisArrow({ position, rotation, size }) {
-  const pointerRef = useRef();
+interface AxisArrowProps {
+  position: Coordinate;
+  rotation: Coordinate;
+  size: number;
+}
+
+function AxisArrow({ position, rotation, size }: AxisArrowProps) {
+  const pointerRef = useRef<Mesh>(null!);
   const { nodes, materials } = useGLTF("/assets/arrow_3d.glb");
 
   useEffect(() => {
-    if (pointerRef) {
-      pointerRef.current.material.color.set("#12e75c");
+    if (pointerRef.current) {
+      const material = pointerRef.current.material as MeshStandardMaterial;
+      material.color.set("#12e75c");
     }
   }, [pointerRef]);
 
   return (
     <group position={position} rotation={rotation} scale={[0.05, 0.2, 0.3]}>
-      <mesh
-        scale={[1, 1 * size, 1]}
-        ref={pointerRef}
-        geometry={nodes.Arrow__0.geometry}
-        material={materials["Scene_-_Root"]}
-      />
+      {nodes.Arrow__0 && nodes.Arrow__0 instanceof Mesh && (
+        <mesh
+          scale={[1, 1 * size, 1]}
+          ref={pointerRef}
+          geometry={nodes.Arrow__0.geometry}
+          material={materials["Scene_-_Root"]}
+        />
+      )}
     </group>
   );
 }
--- src/components/CubePointer/AxisMarker.tsx
+++ src/components/CubePointer/AxisMarker.tsx
@@ -1,67 +1,84 @@
 import React, { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
+import { Mesh, MeshStandardMaterial } from "three";
+import { Coordinate } from "../../../types/cube.ts";
 
 useGLTF.preload("/assets/alphabet_asset.glb");
 
-function AxisX({ position, rotation }) {
-  const pointerRef = useRef();
+interface AxisProps {
+  position: Coordinate;
+  rotation: Coordinate;
+}
+
+function AxisX({ position, rotation }: AxisProps) {
+  const pointerRef = useRef<Mesh>(null!);
   const { nodes } = useGLTF("/assets/alphabet_asset.glb");
 
   useEffect(() => {
-    if (pointerRef) {
-      pointerRef.current.material.color.set("#000000");
+    if (pointerRef.current) {
+      const material = pointerRef.current.material as MeshStandardMaterial;
+      material.color.set("#000000");
     }
-  }, []);
+  }, [pointerRef]);
 
   return (
     <group position={position} rotation={rotation} scale={[0.5, 0.5, 0.5]}>
-      <mesh
-        ref={pointerRef}
-        geometry={nodes.Object_8.geometry}
-        material={nodes.Object_8.material}
-      />
+      {nodes.Object_8 && nodes.Object_8 instanceof Mesh && (
+        <mesh
+          ref={pointerRef}
+          geometry={nodes.Object_8.geometry}
+          material={nodes.Object_8.material}
+        />
+      )}
     </group>
   );
 }
 
 function AxisY({ position, rotation }) {
-  const pointerRef = useRef();
+  const pointerRef = useRef<Mesh>(null!);
   const { nodes } = useGLTF("/assets/alphabet_asset.glb");
 
   useEffect(() => {
-    if (pointerRef) {
-      pointerRef.current.material.color.set("#000000");
+    if (pointerRef.current) {
+      const material = pointerRef.current.material as MeshStandardMaterial;
+      material.color.set("#000000");
     }
-  }, []);
+  }, [pointerRef]);
 
   return (
     <group position={position} rotation={rotation} scale={[0.5, 0.5, 0.5]}>
-      <mesh
-        ref={pointerRef}
-        geometry={nodes.Object_4.geometry}
-        material={nodes.Object_4.material}
-      />
+      {nodes.Object_4 && nodes.Object_4 instanceof Mesh && (
+        <mesh
+          ref={pointerRef}
+          geometry={nodes.Object_4.geometry}
+          material={nodes.Object_4.material}
+        />
+      )}
+      ;
     </group>
   );
 }
 
 function AxisZ({ position, rotation }) {
-  const pointerRef = useRef();
+  const pointerRef = useRef<Mesh>(null!);
   const { nodes } = useGLTF("/assets/alphabet_asset.glb");
 
   useEffect(() => {
-    if (pointerRef) {
-      pointerRef.current.material.color.set("#000000");
+    if (pointerRef.current) {
+      const material = pointerRef.current.material as MeshStandardMaterial;
+      material.color.set("#000000");
     }
-  }, []);
+  }, [pointerRef]);
 
   return (
     <group position={position} rotation={rotation} scale={[0.5, 0.5, 0.5]}>
-      <mesh
-        ref={pointerRef}
-        geometry={nodes.Object_6.geometry}
-        material={nodes.Object_6.material}
-      />
+      {nodes.Object_6 && nodes.Object_6 instanceof Mesh && (
+        <mesh
+          ref={pointerRef}
+          geometry={nodes.Object_6.geometry}
+          material={nodes.Object_6.material}
+        />
+      )}
     </group>
   );
 }
