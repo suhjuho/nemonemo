60c582b0f412f3846b3c0ee4a594c3ee673e9eb4
suhjuho <sspaka0422@gmail.com>
2024-06-29 18:34:10 +0900

    Refactor: separate useEffect with module

--- src/components/Puzzle/Puzzle.tsx
+++ src/components/Puzzle/Puzzle.tsx
@@ -1,28 +1,23 @@
-import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import Cube from "../Cube/Cube.tsx";
 import Scaffold from "../Edge/Scaffold.tsx";
 import DefaultScaffold from "../Edge/DefaultScaffold.tsx";
-import {
-  useAnswerStore,
-  useCubeStatesStore,
-  useCameraPositionStore,
-  useLayerStore,
-} from "../../store/store.tsx";
+import { useAnswerStore, useCameraPositionStore } from "../../store/store.tsx";
 import {
   CUBE_CONSTANT,
   CubeGeometry,
   CubeLineGeometry,
 } from "../../constants/cube.ts";
-import { Puzzle as PuzzleData } from "../../../types/puzzle.ts";
 import {
-  MarkingNumbers,
-  DefaultPuzzle,
-  CubeState,
-} from "../../../types/cube.ts";
+  DifficultyLevel,
+  Puzzle as PuzzleData,
+} from "../../../types/puzzle.ts";
+import { MarkingNumbers, DefaultPuzzle } from "../../../types/cube.ts";
 import revertCoordinate from "../../utils/revertCoordinate.ts";
-import convertCoordinate from "../../utils/convertCoordinate.ts";
+import useSetPuzzleData from "../../utils/useSetPuzzleData.tsx";
+import useSetGameHistory from "../../utils/useSetGameHistory.tsx";
+import useLayerChange from "../../utils/useLayerChange.tsx";
 
 function Puzzle({
   puzzle,
@@ -33,114 +28,18 @@ function Puzzle({
   markingNumbers: MarkingNumbers;
   defaultPuzzle: DefaultPuzzle;
 }) {
-  const { size, answers, colors } = puzzle;
-  const { difficulty, stageNumber } = useParams();
+  const { size, colors } = puzzle;
+  const { difficulty = "easy", stageNumber = "1" } = useParams<{
+    difficulty: DifficultyLevel;
+    stageNumber: string;
+  }>();
 
-  const { setAnswer, isComplete, setIsComplete } = useAnswerStore();
-  const {
-    cubeStates,
-    cubeStatesHistory,
-    historyIndex,
-    setCubeStates,
-    setCubeStatesHistory,
-    setHistoryIndex,
-  } = useCubeStatesStore();
+  const { isComplete } = useAnswerStore();
   const { cameraPosition } = useCameraPositionStore();
-  const { layerDirection, layers, currentLayer, setCurrentLayer, setLayers } =
-    useLayerStore();
 
-  useEffect(() => {
-    const newAnswer: Record<string, boolean> = {};
-    const newCubeStates: Record<string, CubeState> = {};
-
-    Object.entries(answers).forEach((position) => {
-      newAnswer[convertCoordinate(position[0], size).join("")] = true;
-    });
-
-    defaultPuzzle.forEach((position) => {
-      newCubeStates[position.join("")] = {
-        isClicked: false,
-        isRemoved: false,
-        isHidden: false,
-      };
-    });
-
-    const puzzleLayers: { x: number[]; z: number[] } = { x: [], z: [] };
-
-    for (let z = -1 * puzzle.size[2] + 1; z <= puzzle.size[2] - 1; z += 2) {
-      puzzleLayers.z.push(z);
-    }
-
-    for (let x = -1 * puzzle.size[0] + 1; x <= puzzle.size[0] - 1; x += 2) {
-      puzzleLayers.x.push(x);
-    }
-
-    setLayers(puzzleLayers);
-    setCurrentLayer(puzzle.size[2]);
-
-    setAnswer(newAnswer);
-    setIsComplete(false);
-    setCubeStates(newCubeStates);
-    setCubeStatesHistory([JSON.parse(JSON.stringify(newCubeStates))]);
-    setHistoryIndex(0);
-  }, [difficulty, stageNumber, defaultPuzzle]);
-
-  useEffect(() => {
-    function handleCubeHistory(event: KeyboardEvent): void {
-      const isUndo = CUBE_CONSTANT.UNDO_KEYS[event.key];
-      const isRedo = CUBE_CONSTANT.REDO_KEYS[event.key];
-
-      if (isUndo && historyIndex > 0) {
-        setHistoryIndex(historyIndex - 1);
-      }
-
-      if (isRedo && historyIndex < cubeStatesHistory.length - 1) {
-        setHistoryIndex(historyIndex + 1);
-      }
-    }
-
-    window.addEventListener("keydown", handleCubeHistory);
-    return () => window.removeEventListener("keydown", handleCubeHistory);
-  }, [cubeStates, cubeStatesHistory, historyIndex]);
-
-  useEffect(() => {
-    function handleLayerChange(event: KeyboardEvent): void {
-      const isInside = CUBE_CONSTANT.INSIDE_CUBE_KEYS[event.key];
-      const isOutside = CUBE_CONSTANT.OUTSIDE_CUBE_KEYS[event.key];
-
-      const layer =
-        layerDirection === "FRONT" || layerDirection === "BACK"
-          ? layers.z
-          : layers.x;
-
-      if (isInside) {
-        if (CUBE_CONSTANT.INSIDE_DIRECTIONS[layerDirection]) {
-          if (currentLayer > 1) {
-            setCurrentLayer(currentLayer - 1);
-          }
-        } else if (CUBE_CONSTANT.OUTSIDE_DIRECTIONS[layerDirection]) {
-          if (currentLayer < layer.length) {
-            setCurrentLayer(currentLayer + 1);
-          }
-        }
-      }
-
-      if (isOutside) {
-        if (CUBE_CONSTANT.INSIDE_DIRECTIONS[layerDirection]) {
-          if (currentLayer < layer.length) {
-            setCurrentLayer(currentLayer + 1);
-          }
-        } else if (CUBE_CONSTANT.OUTSIDE_DIRECTIONS[layerDirection]) {
-          if (currentLayer > 1) {
-            setCurrentLayer(currentLayer - 1);
-          }
-        }
-      }
-    }
-
-    window.addEventListener("keydown", handleLayerChange);
-    return () => window.removeEventListener("keydown", handleLayerChange);
-  }, [currentLayer, layerDirection, layers]);
+  useSetPuzzleData(puzzle, defaultPuzzle, difficulty, stageNumber);
+  useSetGameHistory();
+  useLayerChange();
 
   return (
     <>
